078f39eb7f924a1d14bcbcc95a3f09bab8987659
OKso <user+github@okso.me>
2014-01-30 09:06:08 +0100

    [dev] Repurposed into Wake Up / Sleep

--- qml/hellozmq/main.qml
+++ qml/hellozmq/main.qml
@@ -10,13 +10,15 @@ Rectangle {
     }
 
     Rectangle {
-        id: hellobutton
-        color: '#FFAAAA'
+        id: wakeup_button
+        color: '#400000'
         height: parent.height / 2
         width: parent.width
 
         Text {
-            text: qsTr("Hello World")
+            text: qsTr("Wake Up !")
+            color: '#FF8000'
+            font.pointSize: 24
             anchors.centerIn: parent
         }
 
@@ -26,18 +28,22 @@ Rectangle {
                 zerospeaker.printMessage("Hello")
                 zerospeaker.playMPD()
             }
+            onPressed: parent.color = '#600000'
+            onReleased: parent.color = '#400000'
         }
     }
 
     Rectangle {
-        id: zerobutton
-        color: '#AAAAFF'
+        id: sleep_button
+        color: '#000040'
         height: parent.height / 2
         width: parent.width
         y: parent.height / 2
 
         Text {
-            text: qsTr("Pause")
+            text: qsTr("Goto sleep...")
+            color: '#0080FF'
+            font.pointSize: 24
             anchors.centerIn: parent
         }
 
@@ -46,8 +52,8 @@ Rectangle {
             onClicked: {
                 zerospeaker.pauseMPD()
             }
-            onPressed: parent.color = '#AAAACC'
-            onReleased: parent.color = '#AAAAFF'
+            onPressed: parent.color = '#000060'
+            onReleased: parent.color = '#000040'
         }
     }
 }
--- zerospeaker.cpp
+++ zerospeaker.cpp
@@ -25,6 +25,22 @@ void ZeroSpeaker::playMPD()
     zmq_connect (requester, "tcp://intercom:5556");
     zmq_send(requester, "do:mpd.play {}", 14, 0);
 
+    requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://intercom:5556");
+    zmq_send(requester, "do:arduino.switch {\"plug\": \"00100\", \"action\": \"on\", \"group\": \"00001\"}", 69, 0);
+
+    requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://intercom:5556");
+    zmq_send(requester, "do:arduino.switch {\"plug\": \"10000\", \"action\": \"on\", \"group\": \"00001\"}", 69, 0);
+
+    requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://intercom:5556");
+    zmq_send(requester, "do:arduino.switch {\"plug\": \"00010\", \"action\": \"on\", \"group\": \"00001\"}", 69, 0);
+
+    requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://intercom:5556");
+    zmq_send(requester, "do:arduino.switch {\"plug\": \"01000\", \"action\": \"on\", \"group\": \"00001\"}", 69, 0);
+
     char buffer [4];
     zmq_recv (requester, buffer, 4, 0);
     buffer[3] = '\0';
@@ -34,10 +50,31 @@ void ZeroSpeaker::playMPD()
 void ZeroSpeaker::pauseMPD()
 {
     printf ("Connecting to hello world server...\n");
+
     void *context = zmq_ctx_new ();
     void *requester = zmq_socket (context, ZMQ_REQ);
     zmq_connect (requester, "tcp://intercom:5556");
     zmq_send(requester, "do:mpd.pause {}", 15, 0);
+
+    context = zmq_ctx_new ();
+    requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://intercom:5556");
+    zmq_send(requester, "do:arduino.switch {\"plug\": \"00100\", \"action\": \"off\", \"group\": \"00001\"}", 70, 0);
+
+    context = zmq_ctx_new ();
+    requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://intercom:5556");
+    zmq_send(requester, "do:arduino.switch {\"plug\": \"10000\", \"action\": \"off\", \"group\": \"00001\"}", 70, 0);
+
+    context = zmq_ctx_new ();
+    requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://intercom:5556");
+    zmq_send(requester, "do:arduino.switch {\"plug\": \"00010\", \"action\": \"off\", \"group\": \"00001\"}", 70, 0);
+
+    context = zmq_ctx_new ();
+    requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://intercom:5556");
+    zmq_send(requester, "do:arduino.switch {\"plug\": \"01000\", \"action\": \"off\", \"group\": \"00001\"}", 70, 0);
 
     char buffer [4];
     zmq_recv (requester, buffer, 4, 0);
